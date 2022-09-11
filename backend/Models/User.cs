e11f33bde00c9e1a5d74ef21eed500797d858cca
Thushan <thushan.perera95@gmail.com>
2022-09-11 10:49:26 +0800

    Reduce some of the unnecessary warnings.

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -6,13 +6,13 @@ namespace backend.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
+    public string Username { get; set; } = null!;
     
     [JsonIgnore]
-    public string Hash { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
+    public string Hash { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     
     [JsonIgnore]
     [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
